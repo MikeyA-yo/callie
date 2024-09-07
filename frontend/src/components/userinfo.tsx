82bede13fe7fb17415eb949627a3c1faf50f744d
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-07 06:57:56 +0100

    better side

--- frontend/src/components/userinfo.tsx
+++ frontend/src/components/userinfo.tsx
@@ -18,14 +18,15 @@ export function UserIntro(){
     useEffect(()=>{
         user.length > 4 && setUserOb(JSON.parse(user))
     }, [user])
+    
     return (
         <>
-          {!user && 
+          {!userOb && 
           <div className="flex flex-col items-center w-full gap-1">
             <h1>Hello user, you don't have a name Set-up, let's do that</h1>
              <div className="flex flex-col w-full gap-2 items-center">
                 <p>Name: </p>
-                <input className="p-2 rounded text-[#3C3D37]" placeholder="Full Name" name="name" onChange={(e)=>{
+                <input className="p-2 rounded text-[#3C3D37]" placeholder="Full Name" name="name" id="n" onChange={(e)=>{
                    setConfig(prev => {
                     return {
                         ...prev,
@@ -34,7 +35,7 @@ export function UserIntro(){
                    }) 
                 }} />
                 <p>Username: </p>
-                <input className="p-2 rounded text-[#3C3D37]" placeholder="kinda like your nick name" name="username" onChange={(e)=>{
+                <input className="p-2 rounded text-[#3C3D37]" placeholder="kinda like your nick name" name="username" id="nn" onChange={(e)=>{
                    setConfig(prev => {
                     return {
                         ...prev,
@@ -44,9 +45,23 @@ export function UserIntro(){
                 }} />
                 <button className="p-2 bg-[#3C3D37] rounded" onClick={()=>{
                     EditUser(JSON.stringify(config))
+                    setUserOb(config);
+                    if (document.getElementById("n") && document.getElementById("nn") ){
+                        let n = document.getElementById("n") as HTMLInputElement;
+                        let nn = document.getElementById("nn") as HTMLInputElement
+                        n.value = ""
+                        nn.value = ""
+                    }
+                    // document.getElementById("n") && document.getElementById("nn") && document.getElementById("n").value
                 }}>Save</button>
              </div>
           </div>
+            }
+            {userOb &&
+            <div>
+                Hey {userOb.name}
+            </div>
+
             }
         </>
     )
