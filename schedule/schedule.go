b2179433fffd03aaa1f70e326c7cb3f8143c67d1
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-09-10 19:16:05 +0100

    what do you think of how i use methods, cool right

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -3,6 +3,7 @@ package schedule
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -27,6 +28,24 @@ type Qhandler struct {
 func NewQhandler() *Qhandler {
 	return &Qhandler{}
 }
-func (q *Qhandler) Query(client *mongo.Client, t string) {
+func (q *Qhandler) Query(client *mongo.Client, t string, coll string) {
 	q.ctx = context.TODO()
+	collection := client.Database("callie").Collection(coll)
+	if t == "find" {
+		collection.FindOne(q.ctx, bson.D{{Key: "roomId", Value: "n"}})
+	}
+}
+
+type RoomDoc struct {
+	RoomId     string `bson:"roomId"`
+	ExpiryDate int    `bson:"expiryDate"`
+	Owner      string `bson:"owner"`
+}
+
+func (q *Qhandler) Find(client *mongo.Client, filter, value string) *RoomDoc {
+	var result *RoomDoc
+	q.ctx = context.TODO()
+	findRes := client.Database("callie").Collection("rooms").FindOne(q.ctx, bson.D{{Key: filter, Value: value}})
+	findRes.Decode(&result)
+	return result
 }
